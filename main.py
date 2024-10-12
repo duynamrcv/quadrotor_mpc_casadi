7e9ffc4d08001dc228c0351de6443a19ca8525d8
Duy-Nam Bui <duynam.robotics@gmail.com>
2024-10-12 21:34:45 +0700

    Add time to data

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from controller import Controller
 from parameters import *
 import numpy as np
 import pickle
-import logging, coloredlogs
-
-coloredlogs.install()
+import time
 
 if __name__ == "__main__":
     quad = Quadrotor()
     controller = Controller(quad, n_nodes=N, dt=DT)
     path = []
     ref = []
+    times = []
 
     cur_time = 0
     total_time = 60
@@ -20,7 +19,10 @@ if __name__ == "__main__":
         x0 = np.concatenate(quad.get_state())
         x_ref = get_reference(cur_time, x0, N, DT)
         ref.append(x_ref[:,1])
+        start = time.time()
         thrust = controller.compute_control_signal(x_ref)
+        times.append(time.time() - start)
+        
         # logging.info("Thrust value [0,1]: {}\t{}\t{}\t{}".format(thrust[0], thrust[1], thrust[2], thrust[3]))
         quad.update(thrust, dt=DT)
         # print(quad.pos)
@@ -31,7 +33,12 @@ if __name__ == "__main__":
     with open(FILE_NAME, 'wb') as file:
         path = np.array(path)
         ref = np.array(ref)
+        times = np.array(times)
+        print("Max processing time: {:.4f}s".format(times.max()))
+        print("Min processing time: {:.4f}s".format(times.min()))
+        print("Mean processing time: {:.4f}s".format(times.mean()))
         data = dict()
         data['path'] = path
         data['ref'] = ref
+        data['times'] = times
         pickle.dump(data, file)
