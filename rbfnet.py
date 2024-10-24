96f08574dba0ddc43313a51fc11efd21f42d91dc
Duy-Nam Bui <duynam.robotics@gmail.com>
2024-10-24 08:03:21 +0700

    Remove example rbf

--- rbfnet.py
+++ rbfnet.py
@@ -33,37 +33,3 @@ class RBFNet:
     # Radial Basis Function (Gaussian)
     def rbf(x, c, s):
         return np.exp(-np.linalg.norm(x - c) ** 2 / (s ** 2))
-
-# Example data: simple 1D function (y = sin(x)) to approximate
-def generate_data():
-    X = np.linspace(-5, 5, 100)  # 100 points between -5 and 5
-    Y = np.sin(X) + np.sin(3*X)
-    return X, Y
-
-if __name__ == "__main__":
-    # Initialize RBF Network
-    rbf_net = RBFNet(num_inputs=1, num_rbfs=20, output_dim=1, learning_rate=0.2)
-
-    # Generate training data
-    X_train, Y_train = generate_data()
-
-    # Test the model on new data
-    Y_pred = []
-    y_cur = 0
-    for i in range(X_train.shape[0]):
-        # print(i)
-        x = X_train[i]
-        y_train = Y_train[i]
-        y_pred = rbf_net.predict(np.array([x]), error=y_train-y_cur)
-        y_cur = y_pred
-        Y_pred.append(y_pred)
-
-    Y_pred = np.array(Y_pred).flatten()
-
-    # Plotting the results
-    import matplotlib.pyplot as plt
-    plt.plot(X_train, Y_train, label="True Function")
-    plt.plot(X_train, Y_pred, label="RBF Network Approximation", linestyle='dashed')
-    plt.plot(X_train, Y_train-Y_pred, label="Estimation Error", linestyle='-.')
-    plt.legend()
-    plt.show()
